3be0bf6fe3f532bc159e4769d962a10bb4ad66fa
Kaloian Doganov <doganov@projectoria.bg>
2011-08-26 15:26:18 +0300

    Add nice layout animation when showing list of apps

--- src/bg/projectoria/appinspector/Main.java
+++ src/bg/projectoria/appinspector/Main.java
@@ -32,6 +32,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.LayoutAnimationController;
+import android.view.animation.TranslateAnimation;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
@@ -55,6 +60,24 @@ public class Main extends ListActivity {
         
         ListAdapter adapter = new AppAdapter(apps);
         setListAdapter(adapter);
+        
+        AnimationSet set = new AnimationSet(true);
+
+        Animation animation = new AlphaAnimation(0.0f, 1.0f);
+        animation.setDuration(50);
+        set.addAnimation(animation);
+
+        animation = new TranslateAnimation(
+            Animation.RELATIVE_TO_SELF, 0.0f,Animation.RELATIVE_TO_SELF, 0.0f,
+            Animation.RELATIVE_TO_SELF, -1.0f,Animation.RELATIVE_TO_SELF, 0.0f
+        );
+        animation.setDuration(100);
+        set.addAnimation(animation);
+
+        LayoutAnimationController controller =
+        	new LayoutAnimationController(set, 0.5f);
+        ListView listView = getListView();        
+        listView.setLayoutAnimation(controller);
     }
     
 	@Override
